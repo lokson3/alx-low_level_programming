12030a15a5a9a3d20a2965e8d86cd8cc5c31db2f
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-10 06:39:18 -0800

    -Wall -pedantic -Werror -Wextra -std=gnu89 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print number from 0 to 9
+ *
+ * Return: (0) Always
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
