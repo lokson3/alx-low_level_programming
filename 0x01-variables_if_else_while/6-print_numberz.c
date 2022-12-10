1dda1e22958c833a3bd6dbd0310827532c04097a
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-10 06:56:31 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print numberz
+ *
+ * Return: (0) Always
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
