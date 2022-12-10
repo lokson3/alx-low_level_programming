3f1cbf087c1025274f6ba0cf6673eb808faee40a
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-10 05:54:01 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print lower and uppercase characters
+ *
+ * Return: (0) Always
+ */
+
+int main(void)
+{
+	char a;
+	char A;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	for (A = 'A'; A <= 'Z'; A++)
+	{
+		putchar(A);
+	}
+	putchar('\n');
+	return (0);
+}
