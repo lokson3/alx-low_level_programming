374c98eee0b7ac12f47793efb0b65c4796450b8c
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-14 08:25:10 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ *
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: addition of a and b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - add two numbers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+
+	a = add(23, 7);
+
+	printf("%d\n", a);
+
+	return (0);
+}
+
