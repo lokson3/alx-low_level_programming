2d51ad2146da12bbc91a8b696fc06cbf14de54cd
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-14 06:29:49 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - absolute value of an integer
+ *
+ * @n: value to be compared
+ *
+ * Return: unasigned int value
+ */
+
+int _abs(int n)
+{
+	int a = abs(n);
+
+
+
+	return (a);
+}
+
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print absolute integer
+ *
+ * Return: (0) Always
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _abs(-10);
+
+	printf("%d\n", r);
+
+	return (0);
+}
+
