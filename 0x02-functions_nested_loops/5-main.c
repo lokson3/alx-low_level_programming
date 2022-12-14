19386ed1a858a6023e93eeb27d2bd8eb4bcb04da
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-14 02:21:49 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - print signs
+ *
+ * Return: (0) Always
+ */
+
+int main(void)
+{
+	print_sign(-2);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - print signs
+ *
+ * @n: value to compare
+ * Return: 1 or 0
+ */
+
+int print_sign(int n)
+{
+	int value;
+
+	if (n > 0)
+	{
+		value = 1;
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		value = 0;
+		_putchar('0');
+	}
+	else
+	{
+		value = -1;
+		_putchar('-');
+	}
+	return (value);
+
+}
+
