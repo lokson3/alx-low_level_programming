7799c102a5f4a59b222c713f6046790f70430490
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-15 07:29:29 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print number for 0 to 9
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	print_numbers();
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+}
