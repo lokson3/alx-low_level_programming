ab4cd8951e21ac16cd61dbfe9b579ca7cfc8f04f
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-15 08:45:14 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - run code
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	print_line(3);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints line
+ * @n: parameter
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
