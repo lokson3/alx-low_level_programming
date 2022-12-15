8f0bd6a8f9ae39580ddba996e85225bdaedf7b1e
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-15 07:13:33 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiply two integers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+
+	a = mul(2, 11);
+	printf("%d\n", a);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiply two intergers
+ *
+ * @a: number 1
+ * @b: number 2
+ * Return: return multiplied value
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
