beab59ced8d91d3afc08b5688731885b524031b4
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-19 04:41:59 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - execute code
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+
+	printf("a = %d, b = %d\n", a, b);
+
+	swap_int(&a, &b);
+
+	printf("a = %d, b = %d\n", a, b);
+	return (0);
+}
