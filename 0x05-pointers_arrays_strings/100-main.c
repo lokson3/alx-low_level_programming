2d8226ba318bfa380bac724c394dc57824bec277
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-20 05:35:23 -0800

    100-atoi

---
+++ 0x05-pointers_arrays_strings/100-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int nb;
+
+	nb = _atoi("98");
+	printf("%d\n", nb);
+	return (0);
+}
