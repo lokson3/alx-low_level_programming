cd29de2067811ab99c34607a7e94ea59543f124b
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-20 05:01:51 -0800

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int array[2];
+
+	array[0] = 22;
+	array[1] = 55;
+	print_array(array, 2);
+	return (0);
+}
