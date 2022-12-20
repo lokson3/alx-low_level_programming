e606a7b8ac372a3991bffd3a9ac4ecde36ef1c32
Oloko Olabisi <olokoolabisi.n@gmail.com>
2022-12-20 04:35:38 -0800

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints even strings
+ * @str: parameter provided
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
