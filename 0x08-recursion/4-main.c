5e36925d0e247c71542d8a6a3a7fc5afd7d20da8
Oloko Olabisi <olokoolabisi.n@gmail.com>
2023-01-05 12:02:08 -0800

    a function that returns the value of x raised to the power of y.

---
+++ 0x08-recursion/4-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check thr code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int r;
+
+	r = _pow_recursion(1, 10);
+	printf("%d\n", r);
+	return (0);
+}
