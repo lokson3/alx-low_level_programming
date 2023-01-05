9511f0d574eb43c0023e9a75613cd8581fdfbaf2
Oloko Olabisi <olokoolabisi.n@gmail.com>
2023-01-05 12:13:58 -0800

    a function that returns the natural square root of a number.

---
+++ 0x08-recursion/5-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _sqrt_recursion(1024);
+	printf("%d\n", r);
+	return (0);
+}
