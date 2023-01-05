49dda4f767c1c10d86fb3db5abccf0efbbad1bdc
Oloko Olabisi <olokoolabisi.n@gmail.com>
2023-01-05 12:21:22 -0800

    a function that returns 1 if the input integer is a prime number, otherwise return 0.

---
+++ 0x08-recursion/6-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int r;
+
+	r = is_prime_number(16);
+	printf("%d\n", r);
+	return (0);
+}
