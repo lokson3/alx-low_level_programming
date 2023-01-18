aa0b33f88f639dc86632de7433ccf31fa7b4754a
Oloko Olabisi <olokoolabisi.n@gmail.com>
2023-01-18 03:53:05 -0800

    a function-like macro SUM(x, y) that computes the sum of the numbers x and y

---
+++ 0x0D-preprocessor/4-main.c
@@ -0,0 +1,17 @@
+#include "4-sum.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int s;
+
+	s = SUM(20, 30);
+	printf("%d\n", s);
+	return (0);
+}
