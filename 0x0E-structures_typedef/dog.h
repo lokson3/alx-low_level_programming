bd230645df0f2176f1699665421b60e75425be6a
Oloko Olabisi <olokoolabisi.n@gmail.com>
2023-01-19 06:44:03 -0800

    a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,8 +13,10 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dot_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 
 
 #endif
